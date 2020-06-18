0819654843d8cf05b8bf0ed440f4388d65d43ef8
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-06-18 00:43:04 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class MP3Importer 
+    attr_accessor :path 
+    def initialize(filepath)
+        @path = filepath 
+    end 
+
+    def files 
+        Dir.entries(path).delete_if do |char| char.size < 3 end 
+    end 
+
+    def import
+        self.files.each do |file|
+            Song.new_by_filename(file)
+        end   
+    end 
+end 
