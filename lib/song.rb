dd16761c4fb54d4bbf2337babcd317c83253ae64
TroyC9 <troy.christy@icloud.com>
2020-06-23 21:47:08 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+
+    @@all = []
+    
+    def self.all
+        @@all
+    end
+
+    attr_accessor :name, :artist 
+
+    def initialize(name)
+        @name = name
+        @@all << self
+        end
+
+    def self.new_by_filename(file_name)
+        artist_name, name = file_name.chomp('.mp3').split(' - ')
+        song = self.new(name)
+        song.artist_name = artist_name
+        song
+    end
+def artist_name=(artist_name)
+    @artist = Artist.find_or_create_by_name(artist_name)
+end
+
+end
