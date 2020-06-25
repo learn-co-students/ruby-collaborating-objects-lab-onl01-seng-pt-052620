648fde837bc6678440a3dc3411c2f19e0eebaa76
patrick-rush <patrick.a.rush@gmail.com>
2020-06-25 16:59:43 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,6 @@ class Artist
     def initialize(name)
         @name = name
         @@all << self
-        @songs = []
     end
 
     def self.all
@@ -16,24 +15,21 @@ class Artist
     end
 
     def add_song(song)
-        @songs << song
+        song.artist = self
     end
 
     def songs
-        @songs
-    end
-
-    def self.find_or_create_by_name(name)
-        result = self.all.find(ifnone = nil) {|artist| artist.name == name}
-        if result == nil
-            self.new(name)
-        else
-            result
+        Song.all.select do |song|
+            song.artist == self
         end
     end
 
+    def self.find_or_create_by_name(name)
+        self.all.find { |artist| artist.name == name } or Artist.new(name)
+    end
+
     def print_songs
-        @songs.each do |song|
+        songs.each do |song|
             puts song.name
         end
     end
--- lib/song.rb
+++ lib/song.rb
@@ -14,23 +14,19 @@ class Song
         @@all
     end
 
-    def artist= (artist)
-        @artist = artist
+    def artist=(artist)
+        @artist = Artist.find_or_create_by_name(artist.name)
     end
 
     def self.new_by_filename(filename)
-        song_info_array = filename.chomp(".mp3").split(" - ")
-        artist = song_info_array[0]
-        song = song_info_array[1]
-        genre = song_info_array[2]
-        new_song = Song.new(song)
+        artist, song, genre = filename.chomp(".mp3").split(" - ")
+        new_song = self.new(song)
         new_song.artist_name = artist
         new_song
     end
 
-    def artist_name= (artist_name)
+    def artist_name=(artist_name)
         artist = Artist.find_or_create_by_name(artist_name)
         self.artist = artist
-        artist.songs << self
     end
 end
